c27062ce5b96077157098bb7ca075e7af487f67c
AFCMS <afcm.contact@gmail.com>
2022-03-16 21:03:59 +0100

    stuff

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	proxy "github.com/HimbeerserverDE/mt-multiserver-proxy"
 )
 
 func init() {
 	fmt.Println("Hello from plugin!")
+	fmt.Println("Players" + fmt.Sprint(proxy.Players()))
 }
