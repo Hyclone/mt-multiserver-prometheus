4ebb5f089c2ec9e86cb0383acaef2fa209f8fcc3
AFCMS <afcm.contact@gmail.com>
2022-03-18 19:01:08 +0100

    FINALLY WORKING

--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.17
 require github.com/prometheus/client_golang v1.12.1
 
 require (
+	github.com/HimbeerserverDE/mt-multiserver-proxy v0.0.0-20220313095549-11343d9aad49 // indirect
+	github.com/HimbeerserverDE/srp v0.0.0-20210331172529-2b5dbec6b82b // indirect
+	github.com/anon55555/mt v0.0.0-20210919124550-bcc58cb3048f // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"time"
 
+	proxy "github.com/HimbeerserverDE/mt-multiserver-proxy"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -14,14 +15,14 @@ func recordMetrics() {
 	go func() {
 		for {
 			fmt.Println("Increasing")
-			playerCount.Inc()
-			time.Sleep(2 * time.Second)
+			playerCount.Set(float64(len(proxy.Players())))
+			time.Sleep(5 * time.Second)
 		}
 	}()
 }
 
 var (
-	playerCount = promauto.NewCounter(prometheus.CounterOpts{
+	playerCount = promauto.NewGauge(prometheus.GaugeOpts{
 		Name: "multiserver_playercount",
 		Help: "The total number of players",
 	})
